ffd9cfe165668005acd5c2de7796adb0662c3fd9
welchwd <100434115+willwelch2000@users.noreply.github.com>
2022-10-02 13:30:25 -0500

    Initial commit

---
+++ src/main/java/com/personal/App.java
@@ -0,0 +1,15 @@
+package com.personal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    static final String driverAddress = "C:\\Users\\willw\\Developer\\chromedriver.exe";
+
+    public static void main( String[] args ) {
+        System.setProperty("webdriver.chrome.driver", driverAddress);
+
+        
+    }
+}
